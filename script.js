e5c449ee033be85b66b3d9aadd9501cc138f238c
Saurav Singh Dhanola <125140898+Sauravdhanola1999@users.noreply.github.com>
2025-02-07 20:53:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+        if (e.target.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+	
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && !input.value && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
+});
